b060c0b79d8b48c2c402ccbc8ff78679787b9d31
qiqitaro <thanajan.saes@bumail.net>
2023-05-07 13:07:40 +0700

    fix something

--- Assets/Scripts/Scriptable/BaitData.cs
+++ Assets/Scripts/Scriptable/BaitData.cs
@@ -8,6 +8,7 @@ using UnityEngine;
     Shrimp = 2,
     Octopus = 3
 }
+[CreateAssetMenu(fileName = "BaitData", menuName = "ScriptableObjects/BaitData", order = 2)]
 public class BaitData : ScriptableObject
 {
     public BaitTypes baitType;
