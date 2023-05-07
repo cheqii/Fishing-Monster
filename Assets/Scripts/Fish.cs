9694abe0707917aabdb6665feca787e236d25e33
loneguss <pitcha.huta@bumail.net>
2023-05-07 11:50:17 +0700

    add fish spawn + behevior

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    [SerializeField] private FishData _fishData;
+    [SerializeField] private float fishMoveSpeed;
+    
+    public enum FishDirection
+    {
+        left,
+        right
+    }
+
+    public FishDirection _Direction;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        fishMoveSpeed = _fishData.FishMoveSpeed + Random.Range(0, _fishData.FishMoveSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += Vector3.left * Time.deltaTime;
+        if (_Direction == FishDirection.left)
+        {
+            this.transform.position += Vector3.left * (Time.deltaTime * fishMoveSpeed);
+        }
+        else
+        {
+            this.transform.position += Vector3.right * Time.deltaTime;
+
+        }
     }
 }
---
+++ Assets/Scripts/Scriptable/FishData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "FishData", menuName = "ScriptableObjects/FishData", order = 1)]
+public class FishData : ScriptableObject
+{
+    public enum FishType
+    {
+        Normal,
+        Predator,
+    }
+
+    [Header("Name")] public string FishName;
+    
+    [Header("Fish Movement Data")]
+    [Range(0.1f, 5.0f)]
+    public float FishMoveSpeed;
+
+    [Header("Fish Manager Data")]
+    
+    [Range(0.1f, 5.0f)]
+    public float fishMoveDelay_ui = 0.2f;
+    
+    public Vector2 fishMoveSpeed_ui = new Vector2(0,100);
+    
+    [Range(0.1f, 20.0f)]
+    public float fishPhaseDelay_ui = 15f;
+}
