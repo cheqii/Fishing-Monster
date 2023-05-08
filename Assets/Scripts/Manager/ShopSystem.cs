63b9119e61d5979931f2ff9f3025813d414e4584
qiqitaro <thanajan.saes@bumail.net>
2023-05-08 19:42:24 +0700

    Unlock System and change bait

--- Assets/Scripts/Manager/ShopSystem.cs
+++ Assets/Scripts/Manager/ShopSystem.cs
@@ -24,6 +24,15 @@ public class ShopSystem : MonoBehaviour
     {
         for (int i = 0; i < items.Length; i++)
         {
+
+            if (!itemData[i].isLocked)
+            {
+                items[i].transform.Find("Lock Icon").gameObject.SetActive(false);
+            }
+            else
+            {
+                items[i].transform.Find("Lock Icon").gameObject.SetActive(true);
+            }
             items[i].transform.Find("Image").GetComponentInChildren<Image>().sprite = itemData[i].itemImage;
             items[i].GetComponentInChildren<TextMeshProUGUI>().text = "$ " + itemData[i].price;
         }
@@ -33,30 +42,41 @@ public class ShopSystem : MonoBehaviour
     {
         for (int i = 0; i < items.Length; i++)
         {
-            var button = items[i].transform.Find("Button");
-            if (coinSystem.CurrentMoney >= itemData[i].price)
+            if (!itemData[i].isLocked)
             {
-                button.GetComponent<Image>().color = Color.green;
+                items[i].transform.Find("Lock Icon").gameObject.SetActive(false);
             }
             else
             {
-                button.GetComponent<Image>().color = Color.red;
-                button.GetComponentInChildren<TextMeshProUGUI>().text = "no money";
+                items[i].transform.Find("Lock Icon").gameObject.SetActive(true);
             }
         }
     }
 
     public void PurchaseItem(int id)
     {
-        if (coinSystem.CurrentMoney >= itemData[id].price)
+        if (coinSystem.CurrentMoney >= itemData[id].price && itemData[id].isLocked)
         {
             Debug.Log("Purchased Item id : " + itemData[id].itemId);
             coinSystem.DecreaseMoney(itemData[id].price);
+            itemData[id].isLocked = false;
+            
+            items[id].transform.Find("Button").GetComponent<Image>().color = Color.grey;
         }
         else
         {
-            Debug.Log("Not Enough Money");
+            if(coinSystem.CurrentMoney < itemData[id].price) Debug.Log("Not enough money");
+            else Debug.Log("Item already purchased");
+            
         }
+    }
+    
+    public void ChangeBait(int id)
+    {
+        // change bait data
+        var rod = GameManager.Instance.Boat.GetComponentInChildren<Rod>();
+        rod.BaitData = itemData[id].baitData;
+        Debug.Log(rod.BaitData);
     }
 
     
--- Assets/Scripts/Scriptable/ItemData.cs
+++ Assets/Scripts/Scriptable/ItemData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 3)]
@@ -10,4 +11,6 @@ public class ItemData : ScriptableObject
     public Sprite itemImage;
     public int itemId;
     public int price;
+    public BaitData baitData;
+    public bool isLocked = true;
 }
